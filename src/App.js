9adf97bc29fa07eed2cc3d8aa6899999febbd4d9
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-03-27 15:08:15 +0300

    updated the search.js file functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from "react";
+// import React from "react";
+import Search from "./Components/Search";
+import axios from "axios";
+
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+
+  const apiurl = "https://www.omdbapi.com/?apikey=e68165a0";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        // console.log(data);
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+
+    // console.log(state.s);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Flicker</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+      </main>
     </div>
   );
 }
